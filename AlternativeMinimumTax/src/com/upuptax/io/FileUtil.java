f942d226989efb9165699aab4543a37a2f8cdcb8
lanliwz <lanliwz@yahoo.com>
2013-01-27 17:35:00 -0500

    add file util

---
+++ AlternativeMinimumTax/src/com/upuptax/io/FileUtil.java
@@ -0,0 +1,56 @@
+package com.upuptax.io;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Properties;
+
+public class FileUtil {
+	public static void save(String formName,String fillingBy,Map<String,Double> form) throws IOException{
+		Path file = Paths.get(System.getProperty("user.home"),fillingBy,formName+".properies");
+		Path folder=Paths.get(System.getProperty("user.home"),fillingBy);
+		if (!Files.exists(folder)){
+			Files.createDirectory(folder);
+		}
+		if(!Files.exists(file)){
+			Files.createFile(file);		
+		
+			
+		}
+		Writer writer = Files.newBufferedWriter(file,Charset.defaultCharset());
+		Properties prop = new Properties();
+		if (form!=null){
+			for (String key:form.keySet()){
+				prop.put(key, String.valueOf(form.get(key)));
+			}
+		}
+		prop.store(writer, null);
+		
+		
+	}
+	public static Map<String,Double> load(String formName,String fillingBy,Map<String,Double> form) throws IOException{
+		Path file = Paths.get(System.getProperty("user.home"),fillingBy,formName+".properies");
+		if(Files.exists(file)){
+			Reader reader= Files.newBufferedReader(file, Charset.defaultCharset());		
+			Properties prop = new Properties();
+			prop.load(reader);
+			if (prop.size()>0 && form!=null){
+				for (Object key:prop.keySet()){
+					form.clear();
+					form.put((String) key, Double.valueOf(prop.getProperty((String)key)));
+				}
+			}
+		
+			
+		}
+	
+		return form;
+		
+	}
+
+}
