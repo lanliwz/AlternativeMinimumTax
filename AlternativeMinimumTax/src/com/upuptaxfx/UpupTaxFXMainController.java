f3a57a3531c7bd4c06bd3a77ac244fd8c10c6fc1
lanliwz <lanliwz@yahoo.com>
2013-03-16 21:38:53 -0400

    add more

---
+++ AlternativeMinimumTax/src/com/upuptaxfx/UpupTaxFXMainController.java
@@ -0,0 +1,22 @@
+package com.upuptaxfx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class UpupTaxFXMainController implements Initializable {
+	@FXML
+	private Label stateTaxAmount;
+	@FXML
+	private Label federalTaxAmount;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
